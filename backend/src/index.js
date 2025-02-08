2339e4cdac45977851b532db8fda3f9a1f6401f1
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-02-08 14:07:28 +0530

    authentication almost done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
 
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js"
@@ -10,7 +11,10 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+// to parse the incoming request with JSON payloads
 app.use(express.json());
+// to parse the cookie
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
