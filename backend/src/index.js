e391dc5985974a8b863c61ba539d398a2b929e29
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-02-09 00:01:46 +0530

    profile page, increasing the payload :memo:

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,15 +13,22 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-// to parse the incoming request with JSON payloads
-app.use(express.json());
-// to parse the cookie
-app.use(cookieParser());
-// since both fronend and backend were running on different ports, we need to enable cors
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true,
+    methods: 'GET, POST, PUT, DELETE',  
+    allowedHeaders: 'Content-Type, Authorization',
 }));
+// to parse the incoming request with JSON payloads
+// app.use(express.json());
+// Increase payload size limit
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb"}));
+// to parse the cookie
+app.use(cookieParser());
+// since both fronend and backend were running on different ports, we need to enable cors
+
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -66,4 +66,19 @@ export const useAuthStore = create((set) => ({
             toast.error(error.response.data.message);
         }
     },
+
+    updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("error in update profile:", error);
+    //   toast.error(error.response.data.message);
+      toast.error("Profile Pic should be less than 10mb");
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
 }));
