4c819b3808ffa13240bd0b4aeaa66e2565207f8b
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-02-11 16:26:25 +0530

    vercel3

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,13 +1,17 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    // origin: ["http://localhost:5173"],
+    origin: [process.env.VITE_REACT_APP_BACKEND_BASEURL || "http://localhost:5173"],
   },
 });
 
