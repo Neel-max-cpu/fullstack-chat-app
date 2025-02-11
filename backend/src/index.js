1da12afb760e4942b6265971172abb1b68fcead7
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-02-11 16:37:13 +0530

    doing

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ const __dirname = path.resolve();
 
 app.use(cors({
     // origin: "http://localhost:5173",
-    origin: [process.env.VITE_REACT_APP_BACKEND_BASEURL || "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
     credentials: true,
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: ["http://localhost:5173"],
-    origin: [process.env.VITE_REACT_APP_BACKEND_BASEURL || "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
   },
 });
 
