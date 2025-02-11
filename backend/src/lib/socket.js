383dd4f853008579b29df52251dc7f642a3cb96c
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-02-11 20:15:58 +0530

    trying6

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -12,6 +12,9 @@ const io = new Server(server, {
   cors: {
     // origin: ["http://localhost:5173"],
     origin: ["https://socialchatapp.vercel.app", "http://localhost:5173"],
+    credentials: true,
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
   },
 });
 
