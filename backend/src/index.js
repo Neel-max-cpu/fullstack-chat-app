95ab88e669fa07901b0f7e2f880bdcfc8f5aa690
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-02-11 20:07:59 +0530

    trying4

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT;
 
 app.use(cors({
     // origin: "http://localhost:5173",
-    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
+    origin: ["https://socialchatapp.vercel.app" || "http://localhost:5173"],
     credentials: true,
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
