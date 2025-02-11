6a581f745c96fbe6fff01b4d04d4eed6c6cfc537
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-02-11 20:04:37 +0530

    trying3

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     // in the production "whatever the url/api"
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : process.env.BACKEND_URL,
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://socialchatappbackend.vercel.app/api",
     // send cookies with all the request -- so wihtcredentials is set to true
     withCredentials: true, 
 });
